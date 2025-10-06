c13612432961052d0e92499d233ef2d905396dcb
CelesteEvertsen <evertsen.celeste@gmail.com>
2025-10-07 01:19:56 +0200

    grip på main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,22 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      
-    </div>
+       <>
+    <main className={styles.main}>
+      <div className={styles.AboveTheFold}>
+        <section className={styles.section}>
+          <h1>Protect Our Planet</h1>
+          <p>
+            Climate change isn’t tomorrow’s problem—it’s ours today. Together,
+            we can build a sustainable future.
+          </p>
+        </section>
+      </div>
+      <div className={styles.ClimaSection}>
+       <p>Her skal  klimaseksjonene være</p>
+        </div>
+    </main>
+    
+    </>
   );
 }
